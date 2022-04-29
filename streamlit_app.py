77e5551b44764061db894fb6527683210c5b98cd
Yannick Rudolf <yannick.rudolf@yahoo.de>
2022-04-29 14:13:14 +0200

    Element vom Datensatz

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,40 @@
 import streamlit as st
 import os
+import pandas as pd
 
 st.title('Garbage-Classification ML4B')
 
+with st.sidebar:
+    st.markdown("[Team Presentation](#team-presentation)", unsafe_allow_html=True)
+    st.markdown("[Projekt Presentation](#projekt-presentation)", unsafe_allow_html=True)
+    st.markdown("[Image classification](#image-classification)", unsafe_allow_html=True)
+
 # TODO - Team Presentation
+st.header('Team Presentation')
 
 # TODO - Projekt Presentation
+st.header('Projekt Presentation')
 
 # TODO - Element from Dataset
+st.header('Image Classification')
+
 dir = './dataSources/'
 
-for file in os.listdir(dir):
-    if os.path.isfile(dir + file):
-        st.markdown('**' + file + '**')
-        st.write(open(dir + file, 'r').read())
+labels = os.listdir(dir + '/Garbage classification/Garbage classification/')
+
+col1, col2 = st.columns(2)
+
+dicts = []
+
+for label in labels:
+    directory = os.path.join(dir + '/Garbage classification/Garbage classification/', label)
+    samples = os.listdir(directory)
+
+    x = {'Type': label, 'Samples': len(samples)}
+    dicts.append(x)
+
+
+df = pd.DataFrame.from_dict(dicts)
+st.dataframe(df)
 
 # TODO - Interactive Element
