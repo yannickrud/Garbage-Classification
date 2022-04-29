d1e5f34b0c7736eaf469384f406d2ce7df8f5236
Yannick Rudolf <yannick.rudolf@yahoo.de>
2022-04-29 09:30:44 +0200

    TODO's

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title('Garbage-Collection ML4B')
+st.title('Garbage-Classification ML4B')
+
+# TODO - Team Presentation
+
+# TODO - Projekt Presentation
+
+# TODO - Element from Dataset
+
+#TODO - Interaktiv Element
