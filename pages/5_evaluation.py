9b5043e67b65de23c3bab0f36eb5877a71186b0c
ChristophLehr <104490847+ChristophLehr@users.noreply.github.com>
2022-06-23 15:54:05 +0200

    evaluation and deployment

--- pages/5_evaluation.py
+++ pages/5_evaluation.py
@@ -8,12 +8,25 @@ st.set_page_config(page_title="Evaluation")
 
 '''
 ## Evaluate Results
+
+
+#### Assessment of data mining results with respect to business success criteria
+
+
+#### Approved models
 '''
 
 '''
 ## Review process
+
+
+#### Review of process
 '''
 
 '''
 ## Determine next steps
+
+#### List of possible actions
+
+#### Decision
 '''
