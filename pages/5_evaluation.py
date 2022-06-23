26e579dde60a5106bec22b39b4108dd350a37549
ChristophLehr <104490847+ChristophLehr@users.noreply.github.com>
2022-06-23 16:18:21 +0200

    evaluation

--- pages/5_evaluation.py
+++ pages/5_evaluation.py
@@ -8,7 +8,11 @@ st.set_page_config(page_title="Evaluation")
 
 '''
 ## Evaluate Results
-
+Evaluate whether the results are fitting with the business objectives or if the results are deficient.
+If time and money is left, you can test the models partly in the real application. The evaluation of the
+data mining results can be used on models which are related to the original business objectives, as well
+as on models which are not necessarily related to the original business objectives - possibly unveiling additional 
+challenges, information or hints for future directions.
 
 #### Assessment of data mining results with respect to business success criteria
 
@@ -18,13 +22,20 @@ st.set_page_config(page_title="Evaluation")
 
 '''
 ## Review process
-
+A deeper analysis of the data mining engagement to catch overlooked important factors, as well as asking yourself
+the questions:
+- Did I build the model correctly?
+- Did we use any prohibited attributes?
+- Are the used attributes suitable for future analyses?
 
 #### Review of process
 '''
 
 '''
 ## Determine next steps
+Now you have to decide - based on the previous evaluation results - whether you finish the project or set up 
+new data mining projects. Factors such as remaining resources and budget should also be analyzed since they are 
+influencing the decision.
 
 #### List of possible actions
 
