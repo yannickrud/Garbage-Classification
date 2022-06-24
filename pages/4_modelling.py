9aebf6409f2e14463f51fded11d80bfc6a1486f0
nicoschu <mail@nicoschunk.de>
2022-06-24 18:21:05 +0200

    generate test design

--- pages/4_modelling.py
+++ pages/4_modelling.py
@@ -21,6 +21,18 @@ the proper size.
 '''
 ## Generate test design
 '''
+'''
+Before we create our final model, we need to check the quality and accuracy with the help of a test model.
+For this we provide the function with the necessary datasets and specify how many epochs the model should run through.
+'''
+st.code('''
+epochs=10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs,
+  callbacks=[early_stopping])
+''')
 
 '''
 ## Build model
