76f3046394b7b10d15d16ef4fdaf75dbcae0f6e4
ChristophLehr <104490847+ChristophLehr@users.noreply.github.com>
2022-06-24 18:18:24 +0200

    updates pages 1 4

--- pages/4_modelling.py
+++ pages/4_modelling.py
@@ -8,6 +8,14 @@ st.set_page_config(page_title="Modelling")
 
 '''
 ## Select Modeling Technique
+The first step in modeling is to select the specific modeling technique.
+#### Modeling Technique 
+The actual modeling technique we used is Convolutional Neural Network (CNN), a neural network has multiple filter kernels per convolutional layer
+, creating layers of feature maps that each get the same input but extract different features due to different weight matrices. 
+
+#### Modeling Assumptions
+The assumptions coming from this model are a required size. This problem is easy fixable since you can simply resize the images if they dont have
+the proper size.
 '''
 
 '''
