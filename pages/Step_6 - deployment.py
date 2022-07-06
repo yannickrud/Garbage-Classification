5983e2a023a2b7c903414179b38422e1aa42f205
ChristophLehr <104490847+ChristophLehr@users.noreply.github.com>
2022-07-06 11:56:36 +0200

    Deployment

--- pages/Step_6 - deployment.py
+++ pages/Step_6 - deployment.py
@@ -27,8 +27,14 @@ results is recommended.
 Writing a final report - whether it includes only a summary of the project, or a final and comprehensive report -
 is the next step.
 #### Final report
-
+The final results we gathered could be better. There is still room for improvement in terms of classifying the objects correctly into the given classes, as well 
+as determining the objects with more confidence into the correct classes. So there are the three following options regarding our project:
+- Improve the algorithm
+- Take the given algorithm
+- Cancelling the project
+As you read in the previous step evaluation, we are happy with the results and determine that the project creating a tutorial for working with datasets was a success.
 #### Final presentation
+Can be attended on friday the 8th of July
 '''
 
 '''
